30efb31c482e1c6228df7705e06e019edaf53b01
Peter Müller <peter@crycode.de>
2020-12-30 14:29:37 +0100

    Added Sentry error reporting in admin, Updated dependencies

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
+import * as SentryIntegrations from '@sentry/integrations';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from '@iobroker/adapter-react/Theme';
 import Utils from '@iobroker/adapter-react/Components/Utils';
 import App from './app';
+
+import * as ioPkg from '../../io-package.json';
 
 let themeName = Utils.getThemeName();
 
@@ -11,7 +15,7 @@ function build(): void {
   ReactDOM.render(
     <MuiThemeProvider theme={theme(themeName)}>
       <App
-        adapterName='canbus'
+        adapterName={ioPkg.common.name}
         onThemeChange={(_theme) => {
           themeName = _theme;
           build();
@@ -20,6 +24,16 @@ function build(): void {
     </MuiThemeProvider>,
     document.getElementById('root'),
   );
+}
+
+if (window.location.host !== 'localhost:3000') {
+  Sentry.init({
+    dsn: ioPkg.common.plugins.sentry.dsn,
+    release: ioPkg.common.name + '@' + ioPkg.common.version,
+    integrations: [
+      new SentryIntegrations.Dedupe()
+    ]
+  });
 }
 
 build();
