c1c711968409e4beb5da07101cfdbc7706325b61
Peter Müller <peter@crycode.de>
2021-03-01 09:17:56 +0100

    Updated typescript, Simplified knownParsers array

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,28 +1,16 @@
-import { ParserBase } from './base';
 import { ParserBoolean } from './boolean';
 import { ParserCustom } from './custom';
 import { ParserNumber } from './number';
 import { ParserString } from './string';
 
 /**
- * Type of `ParserBase` class, needed to create the `ParserLike` interface.
- */
-type ParserBaseClass = typeof ParserBase;
-
-/**
- * Interface which represents any possible subclasses of `ParserBase`.
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ParserLike extends ParserBaseClass { }
-
-/**
  * Array of all known parsers.
- *//*
+ *
  * Each parser which may be used must be listed here!
  * The parsers will be loaded dynamically based on this list in `main.ts`.
  * No need to add new parsers somewhere else than here.
  */
-export const knownParsers: ParserLike[] = [
+export const knownParsers = [
   ParserBoolean,
   ParserNumber,
   ParserString,
